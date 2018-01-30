463ae86979949ed733548e315de1a6a7ca507ca7
doctoretdiez <34245777+doctoretdiez@users.noreply.github.com>
2018-01-30 11:32:17 -0600

    1.30.18 remove dead trees from CSVs before mapping

--- 1.011618_lower48_FIA_TREE_IDextraction.R
+++ 1.011618_lower48_FIA_TREE_IDextraction.R
@@ -5,9 +5,9 @@
 ##############
 
 ############### INPUT: lower 48 state TREE csv files from FIA Datamart
-############### OUTPUT: smaller csv files
+############### OUTPUT: smaller csv files (removing all dead trees)
 ###### lower48_AL_FL.csv (5 location columns of data for all trees in states
-# alphabetically from Alabama to Florida)
+# alphabetically from Alabama to Florida, , plus status code = 1 indicates a living tree)
 ###### lower48_ID_MI.csv
 ###### lower48_MS_OK.csv
 ###### lower48_OR_VT.csv
@@ -25,14 +25,17 @@ setwd("C:/Users/Elizabeth/Desktop/2017_CTS_fellowship/FIA csv")
 # this one will take time to read in
 treeAL <- read.csv("AL_TREE.csv")
 # Because I don't currently need too much information, I will save, plot ID info
-# and species info only.
+# and species info only. Status code is useful because it tells us if the tree is alive (STATUSCD = 1)
 treeAL <- treeAL[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                                          "PLOT", "SPCD")]
+                                          "PLOT", "SPCD", "STATUSCD")]
+# first we want to ensure that all the trees in this sample are live
+treeAL <- treeAL[treeAL$STATUSCD == 1, ]
 
 # Next I will read in the tree info for Arizona and remove most of the columns.
 treeAZ <- read.csv("AZ_TREE.csv")
 treeAZ <- treeAZ[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                     "PLOT", "SPCD")]
+                     "PLOT", "SPCD", "STATUSCD")]
+treeAZ <- treeAZ[treeAZ$STATUSCD == 1, ]
 
 # I will attempt to bind the data frames so far.
 tree_lower48 <- rbind(treeAL, treeAZ)
@@ -41,37 +44,43 @@ rm(treeAL, treeAZ)
 # Continue with Arkansas
 treeAR <- read.csv("AR_TREE.csv")
 treeAR <- treeAR[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                     "PLOT", "SPCD")]
+                     "PLOT", "SPCD", "STATUSCD")]
+treeAR <- treeAR[treeAR$STATUSCD == 1, ]
 tree_lower48 <- rbind(tree_lower48, treeAR)
 rm(treeAR)
 # California
 treeCA <- read.csv("CA_TREE.csv")
 treeCA <- treeCA[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                     "PLOT", "SPCD")]
+                     "PLOT", "SPCD", "STATUSCD")]
+treeCA <- treeCA[treeCA$STATUSCD == 1, ]
 tree_lower48 <- rbind(tree_lower48, treeCA)
 rm(treeCA)
 # Colorado
 treeCO <- read.csv("CO_TREE.csv")
 treeCO <- treeCO[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                     "PLOT", "SPCD")]
+                     "PLOT", "SPCD", "STATUSCD")]
+treeCO <- treeCO[treeCO$STATUSCD == 1, ]
 tree_lower48 <- rbind(tree_lower48, treeCO)
 rm(treeCO)
 #Connecticut
 treeCT <- read.csv("CT_TREE.csv")
 treeCT <- treeCT[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                     "PLOT", "SPCD")]
+                     "PLOT", "SPCD", "STATUSCD")]
+treeCT <- treeCT[treeCT$STATUSCD == 1, ]
 tree_lower48 <- rbind(tree_lower48, treeCT)
 rm(treeCT)
 #Delaware
 treeDE <- read.csv("DE_TREE.csv")
 treeDE <- treeDE[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                     "PLOT", "SPCD")]
+                     "PLOT", "SPCD", "STATUSCD")]
+treeDE <- treeDE[treeDE$STATUSCD == 1, ]
 tree_lower48 <- rbind(tree_lower48, treeDE)
 rm(treeDE)
 #Florida
 treeFL <- read.csv("FL_TREE.csv")
 treeFL <- treeFL[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                     "PLOT", "SPCD")]
+                     "PLOT", "SPCD", "STATUSCD")]
+treeFL <- treeFL[treeFL$STATUSCD == 1, ]
 tree_lower48 <- rbind(tree_lower48, treeFL)
 rm(treeFL)
 
@@ -79,10 +88,12 @@ rm(treeFL)
 write.csv(x = tree_lower48, file = "lower48_AL_FL.csv")
 #remove the dataframe before continuing...
 rm(tree_lower48)
+
 #Georgia
 treeGA <- read.csv("GA_TREE.csv")
 treeGA <- treeGA[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                     "PLOT", "SPCD")]
+                     "PLOT", "SPCD", "STATUSCD")]
+treeGA <- treeGA[treeGA$STATUSCD == 1, ]
 #note that GA is a big file and nearly impossible to read it all in
 # Skip GA
 tree_lower48 <- treeGA
@@ -90,69 +101,78 @@ rm(treeGA)
 # Idaho
 treeID <- read.csv("ID_TREE.csv")
 treeID <- treeID[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                     "PLOT", "SPCD")]
-
+                     "PLOT", "SPCD", "STATUSCD")]
+treeID <- treeID[treeID$STATUSCD == 1, ]
 tree_lower48 <- treeID
 rm(treeID)
 # Illinois
 treeIL <- read.csv("IL_TREE.csv")
 treeIL <- treeIL[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                     "PLOT", "SPCD")]
+                     "PLOT", "SPCD", "STATUSCD")]
+treeIL <- treeIL[treeIL$STATUSCD == 1, ]
 tree_lower48 <- rbind(tree_lower48, treeIL)
 rm(treeIL)
 # Indiana
 treeIN <- read.csv("IN_TREE.csv")
 treeIN <- treeIN[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                     "PLOT", "SPCD")]
+                     "PLOT", "SPCD", "STATUSCD")]
+treeIN <- treeIN[treeIN$STATUSCD == 1, ]
 tree_lower48 <- rbind(tree_lower48, treeIN)
 rm(treeIN)
 # Iowa
 treeIA <- read.csv("IA_TREE.csv")
 treeIA <- treeIA[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                     "PLOT", "SPCD")]
+                     "PLOT", "SPCD","STATUSCD")]
+treeIA <- treeIA[treeIA$STATUSCD == 1, ]
 tree_lower48 <- rbind(tree_lower48, treeIA)
 rm(treeIA)
 # Kansas
 treeKS <- read.csv("KS_TREE.csv")
 treeKS <- treeKS[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                     "PLOT", "SPCD")]
+                     "PLOT", "SPCD", "STATUSCD")]
+treeKS <- treeKS[treeKS$STATUSCD == 1, ]
 tree_lower48 <- rbind(tree_lower48, treeKS)
 rm(treeKS)
 # Kentucky
 treeKY <- read.csv("KY_TREE.csv")
 treeKY <- treeKY[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                     "PLOT", "SPCD")]
+                     "PLOT", "SPCD","STATUSCD")]
+treeKY <- treeKY[treeKY$STATUSCD == 1, ]
 tree_lower48 <- rbind(tree_lower48, treeKY)
 rm(treeKY)
 # Louisiana
 treeLA <- read.csv("LA_TREE.csv")
 treeLA <- treeLA[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                     "PLOT", "SPCD")]
+                     "PLOT", "SPCD", "STATUSCD")]
+treeLA <- treeLA[treeLA$STATUSCD == 1, ]
 tree_lower48 <- rbind(tree_lower48, treeLA)
 rm(treeLA)
 # Maine
 treeME <- read.csv("ME_TREE.csv")
 treeME <- treeME[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                     "PLOT", "SPCD")]
+                     "PLOT", "SPCD", "STATUSCD")]
+treeME <- treeME[treeME$STATUSCD == 1, ]
 tree_lower48 <- rbind(tree_lower48, treeME)
 rm(treeME)
 # Maryland
 treeMD <- read.csv("MD_TREE.csv")
 treeMD <- treeMD[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                     "PLOT", "SPCD")]
+                     "PLOT", "SPCD", "STATUSCD")]
+treeMD <- treeMD[treeMD$STATUSCD == 1, ]
 tree_lower48 <- rbind(tree_lower48, treeMD)
 rm(treeMD)
 # Massachusetts
 treeMA <- read.csv("MA_TREE.csv")
 treeMA <- treeMA[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                     "PLOT", "SPCD")]
+                     "PLOT", "SPCD", "STATUSCD")]
+treeMA <- treeMA[treeMA$STATUSCD == 1, ]
 tree_lower48 <- rbind(tree_lower48, treeMA)
 rm(treeMA)
 # Michigan
 treeMI <- read.csv("MI_TREE.csv")
 treeMI <- treeMI[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                     "PLOT", "SPCD")]
-#This one is really big too
+                     "PLOT", "SPCD", "STATUSCD")]
+treeMI <- treeMI[treeMI$STATUSCD == 1, ]
 tree_lower48 <- rbind(tree_lower48, treeMI)
 rm(treeMI)
 
@@ -163,7 +183,8 @@ rm(tree_lower48)
 # Minnesota
 treeMN <- read.csv("MN_TREE.csv")
 treeMN <- treeMN[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                     "PLOT", "SPCD")]
+                     "PLOT", "SPCD", "STATUSCD")]
+treeMN <- treeMN[treeMN$STATUSCD == 1, ]
 # Had some issues, skip for now.
 tree_lower48 <- treeMN
 rm(treeMN)
@@ -171,79 +192,92 @@ rm(treeMN)
 # Mississippi
 treeMS <- read.csv("MS_TREE.csv")
 treeMS <- treeMS[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                     "PLOT", "SPCD")]
+                     "PLOT", "SPCD", "STATUSCD")]
+treeMS <- treeMS[treeMS$STATUSCD == 1, ]
 tree_lower48 <- treeMS
 rm(treeMS)
 # Missouri
 treeMO <- read.csv("MO_TREE.csv")
 treeMO <- treeMO[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                     "PLOT", "SPCD")]
+                     "PLOT", "SPCD", "STATUSCD")]
+treeMO <- treeMO[treeMO$STATUSCD == 1, ]
 tree_lower48 <- rbind(tree_lower48, treeMO)
 rm(treeMO)
 # Montana
 treeMT <- read.csv("MT_TREE.csv")
 treeMT <- treeMT[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                     "PLOT", "SPCD")]
+                     "PLOT", "SPCD","STATUSCD")]
+treeMT <- treeMT[treeMT$STATUSCD == 1, ]
 tree_lower48 <- rbind(tree_lower48, treeMT)
 rm(treeMT)
 # Nebraska
 treeNE <- read.csv("NE_TREE.csv")
 treeNE <- treeNE[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                     "PLOT", "SPCD")]
+                     "PLOT", "SPCD","STATUSCD")]
+treeNE <- treeNE[treeNE$STATUSCD == 1, ]
 tree_lower48 <- rbind(tree_lower48, treeNE)
 rm(treeNE)
 # Nevada
 treeNV <- read.csv("NV_TREE.csv")
 treeNV <- treeNV[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                     "PLOT", "SPCD")]
+                     "PLOT", "SPCD", "STATUSCD")]
+treeNV <- treeNV[treeNV$STATUSCD == 1, ]
 tree_lower48 <- rbind(tree_lower48, treeNV)
 rm(treeNV)
 # New Hampshire
 treeNH <- read.csv("NH_TREE.csv")
 treeNH <- treeNH[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                     "PLOT", "SPCD")]
+                     "PLOT", "SPCD", "STATUSCD")]
+treeNH <- treeNH[treeNH$STATUSCD == 1, ]
 tree_lower48 <- rbind(tree_lower48, treeNH)
 rm(treeNH)
 #New Jersey
 treeNJ <- read.csv("NJ_TREE.csv")
 treeNJ <- treeNJ[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                     "PLOT", "SPCD")]
+                     "PLOT", "SPCD", "STATUSCD")]
+treeNJ <- treeNJ[treeNJ$STATUSCD == 1, ]
 tree_lower48 <- rbind(tree_lower48, treeNJ)
 rm(treeNJ)
 # New Mexico
 treeNM <- read.csv("NM_TREE.csv")
 treeNM <- treeNM[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                     "PLOT", "SPCD")]
+                     "PLOT", "SPCD", "STATUSCD")]
+treeNM <- treeNM[treeNM$STATUSCD == 1, ]
 tree_lower48 <- rbind(tree_lower48, treeNM)
 rm(treeNM)
 # New York
 treeNY <- read.csv("NY_TREE.csv")
 treeNY <- treeNY[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                     "PLOT", "SPCD")]
+                     "PLOT", "SPCD", "STATUSCD")]
+treeNY <- treeNY[treeNY$STATUSCD == 1, ]
 tree_lower48 <- rbind(tree_lower48, treeNY)
 rm(treeNY)
 # North Carolina
 treeNC <- read.csv("NC_TREE.csv")
 treeNC <- treeNC[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                     "PLOT", "SPCD")]
+                     "PLOT", "SPCD", "STATUSCD")]
+treeNC <- treeNC[treeNC$STATUSCD == 1, ]
 tree_lower48 <- rbind(tree_lower48, treeNC)
 rm(treeNC)
 # North Dakota
 treeND <- read.csv("ND_TREE.csv")
 treeND <- treeND[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                     "PLOT", "SPCD")]
+                     "PLOT", "SPCD", "STATUSCD")]
+treeND <- treeND[treeND$STATUSCD == 1, ]
 tree_lower48 <- rbind(tree_lower48, treeND)
 rm(treeND)
 # Ohio
 treeOH <- read.csv("OH_TREE.csv")
 treeOH <- treeOH[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                     "PLOT", "SPCD")]
+                     "PLOT", "SPCD", "STATUSCD")]
+treeOH <- treeOH[treeOH$STATUSCD == 1, ]
 tree_lower48 <- rbind(tree_lower48, treeOH)
 rm(treeOH)
 # Oklahoma
 treeOK <- read.csv("OK_TREE.csv")
 treeOK <- treeOK[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                     "PLOT", "SPCD")]
+                     "PLOT", "SPCD", "STATUSCD")]
+treeOK <- treeOK[treeOK$STATUSCD == 1, ]
 tree_lower48 <- rbind(tree_lower48, treeOK)
 rm(treeOK)
 
@@ -254,55 +288,64 @@ rm(tree_lower48)
 # Oregon
 treeOR <- read.csv("OR_TREE.csv")
 treeOR <- treeOR[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                     "PLOT", "SPCD")]
+                     "PLOT", "SPCD","STATUSCD")]
+treeOR <- treeOR[treeOR$STATUSCD == 1, ]
 tree_lower48 <- treeOR
 rm(treeOR)
 # Pennsylvania
 treePA <- read.csv("PA_TREE.csv")
 treePA <- treePA[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                     "PLOT", "SPCD")]
+                     "PLOT", "SPCD", "STATUSCD")]
+treePA <- treePA[treePA$STATUSCD == 1, ]
 tree_lower48 <- rbind(tree_lower48, treePA)
 rm(treePA)
 # Rhode Island
 treeRI <- read.csv("RI_TREE.csv")
 treeRI <- treeRI[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                     "PLOT", "SPCD")]
+                     "PLOT", "SPCD", "STATUSCD")]
+treeRI <- treeRI[treeRI$STATUSCD == 1, ]
 tree_lower48 <- rbind(tree_lower48, treeRI)
 rm(treeRI)
 # South Carolina
 treeSC <- read.csv("SC_TREE.csv")
 treeSC <- treeSC[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                     "PLOT", "SPCD")]
+                     "PLOT", "SPCD","STATUSCD")]
+treeSC <- treeSC[treeSC$STATUSCD == 1, ]
 tree_lower48 <- rbind(tree_lower48, treeSC)
 rm(treeSC)
 # South Dakota
 treeSD <- read.csv("SD_TREE.csv")
 treeSD <- treeSD[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                     "PLOT", "SPCD")]
+                     "PLOT", "SPCD","STATUSCD")]
+treeSD <- treeSD[treeSD$STATUSCD == 1, ]
 tree_lower48 <- rbind(tree_lower48, treeSD)
 rm(treeSD)
 # Tennessee
 treeTN <- read.csv("TN_TREE.csv")
 treeTN <- treeTN[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                     "PLOT", "SPCD")]
+                     "PLOT", "SPCD", "STATUSCD")]
+treeTN <- treeTN[treeTN$STATUSCD == 1, ]
 tree_lower48 <- rbind(tree_lower48, treeTN)
 rm(treeTN)
 # Texas
 treeTX <- read.csv("TX_TREE.csv")
 treeTX <- treeTX[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                     "PLOT", "SPCD")]
+                     "PLOT", "SPCD", "STATUSCD")]
+treeTX <- treeTX[treeTX$STATUSCD == 1, ]
 tree_lower48 <- rbind(tree_lower48, treeTX)
 rm(treeTX)
 # Utah
 treeUT <- read.csv("UT_TREE.csv")
 treeUT <- treeUT[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                     "PLOT", "SPCD")]
+                     "PLOT", "SPCD","STATUSCD")]
+treeUT <- treeUT[treeUT$STATUSCD == 1, ]
 tree_lower48 <- rbind(tree_lower48, treeUT)
 rm(treeUT)
 # Vermont
 treeVT <- read.csv("VT_TREE.csv")
 treeVT <- treeVT[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                     "PLOT", "SPCD")]
+                     "PLOT", "SPCD","STATUSCD")]
+treeVT <- treeVT[treeVT$STATUSCD == 1, ]
 tree_lower48 <- rbind(tree_lower48, treeVT)
 rm(treeVT)
 
@@ -313,33 +356,36 @@ rm(tree_lower48)
 # Virginia
 treeVA <- read.csv("VA_TREE.csv")
 treeVA <- treeVA[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                     "PLOT", "SPCD")]
+                     "PLOT", "SPCD","STATUSCD")]
+treeVA <- treeVA[treeVA$STATUSCD == 1, ]
 tree_lower48 <- treeVA
 rm(treeVA)
 # Washington
 treeWA <- read.csv("WA_TREE.csv")
 treeWA <- treeWA[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                     "PLOT", "SPCD")]
+                     "PLOT", "SPCD", "STATUSCD")]
+treeWA <- treeWA[treeWA$STATUSCD == 1, ]
 tree_lower48 <- rbind(tree_lower48, treeWA)
 rm(treeWA)
 # West Virginia
 treeWV <- read.csv("WV_TREE.csv")
 treeWV <- treeWV[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                     "PLOT", "SPCD")]
+                     "PLOT", "SPCD","STATUSCD")]
+treeWV <- treeWV[treeWV$STATUSCD == 1, ]
 tree_lower48 <- rbind(tree_lower48, treeWV)
 rm(treeWV)
 # Wisconsin
 treeWI <- read.csv("WI_TREE.csv")
 treeWI <- treeWI[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                     "PLOT", "SPCD")]
-####too much data? include for now...
-
+                     "PLOT", "SPCD", "STATUSCD")]
+treeWI <- treeWI[treeWI$STATUSCD == 1, ]
 tree_lower48 <- rbind(tree_lower48, treeWI)
 rm(treeWI)
 #Wyoming
 treeWY <- read.csv("WY_TREE.csv")
 treeWY <- treeWY[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                     "PLOT", "SPCD")]
+                     "PLOT", "SPCD", "STATUSCD")]
+treeWY <- treeWY[treeWY$STATUSCD == 1, ]
 tree_lower48 <- rbind(tree_lower48, treeWY)
 rm(treeWY)
 
