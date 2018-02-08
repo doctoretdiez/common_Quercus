bd2f991671fa34c314c398a9a4a5886f85e65507
doctoretdiez <34245777+doctoretdiez@users.noreply.github.com>
2018-02-08 10:15:39 -0600

    add multi-panel nichemap

--- 9.020718_common_niche_maps.R
+++ 9.020718_common_niche_maps.R
@@ -110,3 +110,13 @@ common <- rbind(common, rubra_fia_prep)
 common <- rbind(common, rubra_gbif_prep)
 
 # and now we have a new dataframe for making plots in ggplot2!
+
+png(filename = "Q_rubra_gbif_ppt_met.png", width = 675, height = 556)
+ggplot(data = common, aes(x = annual_ppt, y = mean_annual_temp)) +
+  stat_bin_hex() + facet_grid(species~source) +
+  ggtitle("Common Quercus nichemaps")
+  labs(y = "mean annual temperature (degrees C)", 
+       x = "annual precipitation (mm)")
+dev.off()
+
+
