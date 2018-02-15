8a7d67fabd42ce22751878ed88fc0a3d710d9956
Elizabeth <doctoretdiez@comcast.net>
2018-02-15 12:05:21 -0600

    reduce previous code to function
    remove repetitive code that has been shortened with the new function

--- 2.011718_obtaining_commonFIA.R
+++ 2.011718_obtaining_commonFIA.R
@@ -12,6 +12,8 @@
 ###### lower48_OR_VT.csv
 ###### lower48_VA_WY.csv
 ######
+# Q. alba spcd == 802
+# Q. rubra spcd == 833
 ############### OUTPUT: smaller csv files
 ###### Q_alba_46.csv (each row corresponds to a survey plot in 46 statesin which
 # Quercus alba has been found, including longitude, latitude and density of the plot)
@@ -19,6 +21,12 @@
 ###### 
 ###### *excludes Q alba and Q rubra from Georgia and Minnesota
 
+#
+# Note. condensed state csvs have been previously formatted when looking to find the 
+# location of certain species for mapping purposes very quickly. However, if we need
+# to go back and find additional tree data, such as its diameter, then pull from the
+# raw state csvs
+#
 
 # read in plot data, which includes location according to lat and lon
 # be patient. it takes a while.
@@ -30,41 +38,46 @@ treeALFL <- read.csv("../../lower48_AL_FL.csv")
 
 # Now both of the above hold more information than we need, so let's extract some data
 
-# From 'treeXX' we want identifying codes INVYR, STATECD, UNITCD, COUNTYCD, PLOT, 
 # Quercus alba (species code is 802)
 #
-alba <- treeALFL[which(treeALFL$SPCD==802), c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                                          "PLOT")]
+alba <- 802
+# Quercus rubra (species code is 833)
+rubra <- 833
+# now read in our function to be more efficient.
 
-# Next we will match up the location IDs and merge the 'treeXX' and 'plot' data frames 
-alba_loc <- merge(alba, plot, by = c("INVYR", "STATECD", "UNITCD", 
-                                     "COUNTYCD", "PLOT"), all = F)
-# So we have a good amount of rows to work with, so let's remove unnecessary columns
-# we can remove them by renaming our dataframe and extracting only the columns of interest
-alba_loc <- alba_loc[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                         "PLOT", "LAT", "LON")]
+# run this print function first to examine the available occurrence data. 
+# Be careful if there are a lot of occurrences
+FIA.species.loc.print <- function(df.in, spp) {
+  # pare data frame down to species of interest only
+    species <- df.in[which(df.in$SPCD==spp), c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
+                                             "PLOT")]  
+  print(species)
+  # Next we will match up the location IDs and merge the 'treeXX' and 'plot' data frames 
+  species_loc <- merge(species, plot, by = c("INVYR", "STATECD", "UNITCD", 
+                                             "COUNTYCD", "PLOT"), all = F)
+  # and only keep lat and lon from PLOT
+  species_loc <- species_loc[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
+                                 "PLOT", "LAT", "LON")]
+  # STEP 2: find density data and make new smaller dataframe 
+  # with all unique plots with this species and number them with ID numbers.
+  u <- unique(species_loc[, c('INVYR','STATECD','UNITCD', 'COUNTYCD', 'PLOT', 'LAT', 'LON')])
+  ID <- seq(from = 1, to = length(u$INVYR), by = 1)
+  u_plot <- data.frame(u, ID)
+  
+  density_test <- merge(u_plot, species, by = c("INVYR", "UNITCD", "COUNTYCD", "PLOT", "STATECD"), all = F)
+  t <- as.numeric(table(density_test$ID))
+  u_plot$FIA_plot_density <- t
+  # u_plot will show all unique FIA plots with our species of interest, as well as
+  # the density of the plot (number of individuals of our species tallied in this single survey plot)
+  print(u_plot)
+  
+}
 
-# We just have to add in density here. First make a dataframe 
-# with all unique plots for this state XX and number them with ID numbers.
-u <- unique(alba_loc[,c('INVYR','STATECD','UNITCD', 'COUNTYCD', 'PLOT', 'LAT', 'LON')])
-ID <- seq(from = 1, to = length(u$INVYR), by = 1)
-u_plot <- data.frame(u, ID)
-
-density_test <- merge(u_plot, alba, by = c("INVYR", "UNITCD", "COUNTYCD", "PLOT", "STATECD"), all = F)
-t <- as.numeric(table(density_test$ID))
-u_plot$density <- t
-
-rm(treeALFL)
-
-
-#######
-# to generalize the above, I can write a function:
-# first, load all TREE csvs and then combine them.
-
-FIA.species.loc <- function(df.in, spp) {
+# run this write function second, once ready to compile the occurrence data
+FIA.species.loc.write <- function(df.in, spp) {
   
   species <- df.in[which(df.in$SPCD==spp), c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                                                   "PLOT", "STATUSCD", "DIA")]  
+                                             "PLOT")]  
   print(species)
   
   species_loc <- merge(species, plot, by = c("INVYR", "STATECD", "UNITCD", 
@@ -82,231 +95,73 @@ FIA.species.loc <- function(df.in, spp) {
   u_plot$FIA_plot_density <- t
   print(u_plot)
   
-  write.csv(x = u_plot, file = paste0(spp, "_48.csv"))
+  write.csv(x = u_plot, file = paste0(spp, "", df.in, ".csv"))
 }
 
-FIA.species.loc(df.in = treeAL, spp = 844)
+FIA.species.loc.write(treeALFL, alba)
+FIA.species.loc.write(treeALFL, rubra)
+rm(treeALFL)
 
-write.csv(x = u_plot, file = c(spp, "_48.csv_test"))
-
-
-##### Now we can redo the steps from lines 22 to 41. First let's read in a new state.
-
+##### Now we can rerun the function with another set of tree data.
 treeIDMI <- read.csv("../../lower48_ID_MI.csv")
+FIA.species.loc.write(treeIDMI, alba)
+FIA.species.loc.write(treeIDMI, rubra)
+rm(treeIDMI)
 
-#REVIEW: pare list down to Quercus alba only, merge with plot info, and only keep lat and lon
-alba <- treeIDMI[which(treeIDMI$SPCD==802), c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                                          "PLOT")]
+# Repeat again
+treeMSOK <- read.csv("../../lower48_MS_OK.csv")
+FIA.species.loc.write(treeMSOK, alba)
+FIA.species.loc.write(treeMSOK, rubra)
+rm(treeMSOK)
 
-alba_loc <- merge(alba, plot, by = c("INVYR", "STATECD", "UNITCD", 
-                                     "COUNTYCD", "PLOT"), all = F)
+# Repeat
+treeORVT <- read.csv("../../lower48_OR_VT.csv")
+FIA.species.loc.write(treeORVT, alba)
+FIA.species.loc.write(treeORVT, rubra)
+rm(treeORVT)
 
-alba_loc <- alba_loc[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                         "PLOT", "LAT", "LON")]
-
-# STEP 2: find density data and make new smaller dataframe
-u <- unique(alba_loc[,c('INVYR','STATECD','UNITCD', 'COUNTYCD', 'PLOT', 'LAT', 'LON')])
-ID <- seq(from = 1, to = length(u$INVYR), by = 1)
-u_plot_attach <- data.frame(u, ID)
-
-density_test <- merge(u_plot_attach, alba, by = c("INVYR", "UNITCD", "COUNTYCD", "PLOT", "STATECD"), all = F)
-t <- as.numeric(table(density_test$ID))
-u_plot_attach$density <- t
+# Repeat
+treeVAWY <- read.csv("../../lower48_VA_WY.csv")
+FIA.species.loc.write(treeVAWY, alba)
+FIA.species.loc.write(treeVAWY, rubra)
+rm(treeVAWY)
 
 # Now here is the new part, bind the observations from 'u_plot_attach' to existing 'u_plot'
 
 u_plot <- rbind(u_plot, u_plot_attach)
-rm(treeIDMI)
-# Repeat
-
-treeMSOK <- read.csv("../../lower48_MS_OK.csv")
-
-alba <- treeMSOK[which(treeMSOK$SPCD==802), c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                                              "PLOT")]
-alba_loc <- merge(alba, plot, by = c("INVYR", "STATECD", "UNITCD", 
-                                     "COUNTYCD", "PLOT"), all = F)
-alba_loc <- alba_loc[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                         "PLOT", "LAT", "LON")]
-
-u <- unique(alba_loc[,c('INVYR','STATECD','UNITCD', 'COUNTYCD', 'PLOT', 'LAT', 'LON')])
-ID <- seq(from = 1, to = length(u$INVYR), by = 1)
-u_plot_attach <- data.frame(u, ID)
-
-density_test <- merge(u_plot_attach, alba, by = c("INVYR", "UNITCD", "COUNTYCD", "PLOT", "STATECD"), all = F)
-t <- as.numeric(table(density_test$ID))
-u_plot_attach$density <- t
-
-u_plot <- rbind(u_plot, u_plot_attach)
-rm(treeMSOK)
-
-# Repeat
-
-treeORVT <- read.csv("../../lower48_OR_VT.csv")
-
-alba <- treeORVT[which(treeORVT$SPCD==802), c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                                              "PLOT")]
-alba_loc <- merge(alba, plot, by = c("INVYR", "STATECD", "UNITCD", 
-                                     "COUNTYCD", "PLOT"), all = F)
-alba_loc <- alba_loc[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                         "PLOT", "LAT", "LON")]
-
-u <- unique(alba_loc[,c('INVYR','STATECD','UNITCD', 'COUNTYCD', 'PLOT', 'LAT', 'LON')])
-ID <- seq(from = 1, to = length(u$INVYR), by = 1)
-u_plot_attach <- data.frame(u, ID)
-
-density_test <- merge(u_plot_attach, alba, by = c("INVYR", "UNITCD", "COUNTYCD", "PLOT", "STATECD"), all = F)
-t <- as.numeric(table(density_test$ID))
-u_plot_attach$density <- t
-
-u_plot <- rbind(u_plot, u_plot_attach)
-rm(treeORVT)
-
-# Repeat
-
-treeVAWY <- read.csv("../../lower48_VA_WY.csv")
-
-alba <- treeVAWY[which(treeVAWY$SPCD==802), c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                                              "PLOT")]
-alba_loc <- merge(alba, plot, by = c("INVYR", "STATECD", "UNITCD", 
-                                     "COUNTYCD", "PLOT"), all = F)
-alba_loc <- alba_loc[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                         "PLOT", "LAT", "LON")]
-
-u <- unique(alba_loc[,c('INVYR','STATECD','UNITCD', 'COUNTYCD', 'PLOT', 'LAT', 'LON')])
-ID <- seq(from = 1, to = length(u$INVYR), by = 1)
-u_plot_attach <- data.frame(u, ID)
-
-density_test <- merge(u_plot_attach, alba, by = c("INVYR", "UNITCD", "COUNTYCD", "PLOT", "STATECD"), all = F)
-t <- as.numeric(table(density_test$ID))
-u_plot_attach$density <- t
-
-u_plot <- rbind(u_plot, u_plot_attach)
-rm(treeVAWY)
 
 
 # Note that we are still missing GA, MN and there may be an issue with WI
 # save for now as alba_46
 write.csv(x = u_plot, file = "Q_alba_46.csv")
 
-#
-#
-# Repeat for Quercus rubra below
-#
-#
-
-
-
-treeALFL <- read.csv("../../lower48_AL_FL.csv")
-
-# Quercus rubra (species code is 833)
-
-rubra <- treeALFL[which(treeALFL$SPCD==833), c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                                              "PLOT")]
-rubra_loc <- merge(rubra, plot, by = c("INVYR", "STATECD", "UNITCD", 
-                                     "COUNTYCD", "PLOT"), all = F)
-rubra_loc <- rubra_loc[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                         "PLOT", "LAT", "LON")]
-
-# We just have to add in density here. First make a dataframe 
-u <- unique(rubra_loc[,c('INVYR','STATECD','UNITCD', 'COUNTYCD', 'PLOT', 'LAT', 'LON')])
-ID <- seq(from = 1, to = length(u$INVYR), by = 1)
-u_plot <- data.frame(u, ID)
-
-density_test <- merge(u_plot, rubra, by = c("INVYR", "UNITCD", "COUNTYCD", "PLOT", "STATECD"), all = F)
-t <- as.numeric(table(density_test$ID))
-u_plot$density <- t
-
-rm(treeALFL)
-
-# next
-treeIDMI <- read.csv("../../lower48_ID_MI.csv")
-
-#REVIEW: pare list down to Quercus alba only, merge with plot info, and only keep lat and lon
-rubra <- treeIDMI[which(treeIDMI$SPCD==833), c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                                              "PLOT")]
-rubra_loc <- merge(rubra, plot, by = c("INVYR", "STATECD", "UNITCD", 
-                                     "COUNTYCD", "PLOT"), all = F)
-rubra_loc <- rubra_loc[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                         "PLOT", "LAT", "LON")]
-
-# STEP 2: find density data and make new smaller dataframe
-u <- unique(rubra_loc[,c('INVYR','STATECD','UNITCD', 'COUNTYCD', 'PLOT', 'LAT', 'LON')])
-ID <- seq(from = 1, to = length(u$INVYR), by = 1)
-u_plot_attach <- data.frame(u, ID)
-
-density_test <- merge(u_plot_attach, rubra, by = c("INVYR", "UNITCD", "COUNTYCD", "PLOT", "STATECD"), all = F)
-t <- as.numeric(table(density_test$ID))
-u_plot_attach$density <- t
-
-# Now here is the new part, bind the observations from 'u_plot_attach' to existing 'u_plot'
-
-u_plot <- rbind(u_plot, u_plot_attach)
-rm(treeIDMI)
-# Repeat
-
-treeMSOK <- read.csv("../../lower48_MS_OK.csv")
-
-rubra <- treeMSOK[which(treeMSOK$SPCD==833), c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                                              "PLOT")]
-rubra_loc <- merge(rubra, plot, by = c("INVYR", "STATECD", "UNITCD", 
-                                     "COUNTYCD", "PLOT"), all = F)
-rubra_loc <- rubra_loc[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                         "PLOT", "LAT", "LON")]
-
-u <- unique(rubra_loc[,c('INVYR','STATECD','UNITCD', 'COUNTYCD', 'PLOT', 'LAT', 'LON')])
-ID <- seq(from = 1, to = length(u$INVYR), by = 1)
-u_plot_attach <- data.frame(u, ID)
-
-density_test <- merge(u_plot_attach, rubra, by = c("INVYR", "UNITCD", "COUNTYCD", "PLOT", "STATECD"), all = F)
-t <- as.numeric(table(density_test$ID))
-u_plot_attach$density <- t
-
-u_plot <- rbind(u_plot, u_plot_attach)
-rm(treeMSOK)
-
-# Repeat
-
-treeORVT <- read.csv("../../lower48_OR_VT.csv")
-
-rubra <- treeORVT[which(treeORVT$SPCD==833), c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                                               "PLOT")]
-rubra_loc <- merge(rubra, plot, by = c("INVYR", "STATECD", "UNITCD", 
-                                       "COUNTYCD", "PLOT"), all = F)
-rubra_loc <- rubra_loc[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                           "PLOT", "LAT", "LON")]
-
-u <- unique(rubra_loc[,c('INVYR','STATECD','UNITCD', 'COUNTYCD', 'PLOT', 'LAT', 'LON')])
-ID <- seq(from = 1, to = length(u$INVYR), by = 1)
-u_plot_attach <- data.frame(u, ID)
-
-density_test <- merge(u_plot_attach, rubra, by = c("INVYR", "UNITCD", "COUNTYCD", "PLOT", "STATECD"), all = F)
-t <- as.numeric(table(density_test$ID))
-u_plot_attach$density <- t
-
-u_plot <- rbind(u_plot, u_plot_attach)
-rm(treeORVT)
-
-# Repeat
-
-treeVAWY <- read.csv("../../lower48_VA_WY.csv")
-
-rubra <- treeVAWY[which(treeVAWY$SPCD==833), c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                                               "PLOT")]
-rubra_loc <- merge(rubra, plot, by = c("INVYR", "STATECD", "UNITCD", 
-                                       "COUNTYCD", "PLOT"), all = F)
-rubra_loc <- rubra_loc[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
-                           "PLOT", "LAT", "LON")]
-
-u <- unique(rubra_loc[,c('INVYR','STATECD','UNITCD', 'COUNTYCD', 'PLOT', 'LAT', 'LON')])
-ID <- seq(from = 1, to = length(u$INVYR), by = 1)
-u_plot_attach <- data.frame(u, ID)
-
-density_test <- merge(u_plot_attach, rubra, by = c("INVYR", "UNITCD", "COUNTYCD", "PLOT", "STATECD"), all = F)
-t <- as.numeric(table(density_test$ID))
-u_plot_attach$density <- t
-
-u_plot <- rbind(u_plot, u_plot_attach)
-rm(treeVAWY)
 
 # Write a new csv with the 46 states included
 write.csv(x = u_plot, file = "Q_rubra_46.csv")
+
+
+##For further examination of specific trees, go back to the raw state CSVs
+FIA.species.loc <- function(df.in, spp) {
+  
+  species <- df.in[which(df.in$SPCD==spp), c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
+                                             "PLOT", "STATUSCD", "DIA")]  
+  print(species)
+  
+  species_loc <- merge(species, plot, by = c("INVYR", "STATECD", "UNITCD", 
+                                             "COUNTYCD", "PLOT"), all = F)
+  
+  species_loc <- species_loc[, c("INVYR", "STATECD", "UNITCD", "COUNTYCD",
+                                 "PLOT", "LAT", "LON")]
+  
+  u <- unique(species_loc[, c('INVYR','STATECD','UNITCD', 'COUNTYCD', 'PLOT', 'LAT', 'LON')])
+  ID <- seq(from = 1, to = length(u$INVYR), by = 1)
+  u_plot <- data.frame(u, ID)
+  
+  density_test <- merge(u_plot, species, by = c("INVYR", "UNITCD", "COUNTYCD", "PLOT", "STATECD"), all = F)
+  t <- as.numeric(table(density_test$ID))
+  u_plot$FIA_plot_density <- t
+  print(u_plot)
+  
+  write.csv(x = u_plot, file = paste0(spp, "", db.in, ".csv"))
+}
+
